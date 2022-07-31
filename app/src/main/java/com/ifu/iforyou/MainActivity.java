a6e3b5bf796dfff0c4fa1eec05e26176881ca9ce
Mathula <mathula2504@gmail.com>
2022-07-31 19:59:16 +0100

    Login Screen Design Final

--- app/src/main/java/com/ifu/iforyou/MainActivity.java
+++ app/src/main/java/com/ifu/iforyou/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ifu.iforyou;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        LoginFragment loginFragment = new LoginFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.loginFrameLayout,loginFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
     }
 }
