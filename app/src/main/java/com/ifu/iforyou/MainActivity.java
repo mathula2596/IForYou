9e60de6c71c78ae3f18120c80c0b1a7fa1493450
Mathula <mathula2504@gmail.com>
2022-08-06 14:46:14 +0100

    Forgot Password Design and Buttons

--- app/src/main/java/com/ifu/iforyou/MainActivity.java
+++ app/src/main/java/com/ifu/iforyou/MainActivity.java
@@ -4,20 +4,30 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginFragment loginFragment;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        hide the action top bar in login screen
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        RegisterFragment registerFragment = new RegisterFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.loginFrameLayout,registerFragment);
-        fragmentTransaction.addToBackStack(null);
+       
+
+        loginFragment = new LoginFragment();
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.homeFrameLayout,loginFragment);
         fragmentTransaction.commit();
 
     }
