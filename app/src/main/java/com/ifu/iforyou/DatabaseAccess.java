e58b714918ab158808eb71ea29072096ab53744f
Mathula <mathula2504@gmail.com>
2022-08-07 17:09:27 +0100

    Register Backend Finished

--- app/src/main/java/com/ifu/iforyou/DatabaseAccess.java
+++ app/src/main/java/com/ifu/iforyou/DatabaseAccess.java
@@ -1,9 +1,18 @@
 package com.ifu.iforyou;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View;
+import android.widget.Toast;
+
+import com.scottyab.aescrypt.AESCrypt;
+
+import java.security.GeneralSecurityException;
 
 public class DatabaseAccess {
     private SQLiteOpenHelper openHelper;
@@ -39,5 +48,45 @@ public class DatabaseAccess {
         }
     }
 
+    public boolean register(String universityId, String firstname, String lastname, String email,
+                         String password, String userRole, String courseId, String degreeLevel,
+                         String batchNo, String status) throws GeneralSecurityException {
+
+        String encryptedPassword = AESCrypt.encrypt(universityId,password);
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("universityId",universityId);
+        contentValues.put("firstName",firstname);
+        contentValues.put("lastName",lastname);
+        contentValues.put("email",email);
+        contentValues.put("password",encryptedPassword);
+        contentValues.put("userRole",userRole);
+        contentValues.put("courseId",courseId);
+        contentValues.put("degreeLevel",degreeLevel);
+        contentValues.put("batchNo",batchNo);
+        contentValues.put("status",status);
+
+        long result = db.insert("users", null,contentValues);
+        if(result==-1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /*public void decrypt(View view) throws GeneralSecurityException {
+        String encrpyted = AESCrypt.decrypt(et_key.getText().toString(), et_value.getText().toString());
+        ClipboardManager clipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText("lable", encrpyted);
+        clipboardManager.setPrimaryClip(clipData);
+        Toast.makeText(this, "Your Message Was Copied to clip board", Toast.LENGTH_SHORT).show();
+        et_value.setText("");
+        et_key.setText("");
+        message.setText(String.format("You Decrypted key is (Copied To Clipboard) : %s", encrpyted));
+
+
+    }*/
 
 }
--- app/src/main/java/com/ifu/iforyou/RegisterFragment.java
+++ app/src/main/java/com/ifu/iforyou/RegisterFragment.java
@@ -5,18 +5,43 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.security.GeneralSecurityException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class RegisterFragment extends Fragment {
 
-    private Button btnLogin;
+    private Button btnLogin, btnRegister;
     private View view;
     private FragmentTransaction fragmentTransaction;
     private LoginFragment loginFragment;
+    private RadioButton rdbStudent, rdbLecturer;
+    private TextInputLayout universityId, firstName, lastName, email, password, confirmPassword,
+    courseBorder,batchNumberBorder,degreeLevelBorder;
+    private AutoCompleteTextView course, degreeLevel, batchNumber;
+    private String [] courseName = {"Computing Systems", "Computer Science", "Data Science",
+            "Cyber Security", "Engineering"};
+    private String [] degree = {"Undergraduate","Masters","PhD"};
+    private String [] batch = {"DS2021Jan","DS2021Sep","Comp2021Jan","Comp2021Sep","Comp2022Jan",
+            "Comp2022Sep"};
+    private ArrayAdapter<String> arrayAdapter;
+    private String status = "Active";
+    private String userRole = "Student";
+    private boolean validPassword, validUniversityID,validDropdown, validEmail, validField = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,11 +49,147 @@ public class RegisterFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        btnLogin = view.findViewById(R.id.btnLogin);
+        course = view.findViewById(R.id.course);
+        arrayAdapter = new ArrayAdapter<>(view.getContext(),R.layout.list_item,courseName);
+        course.setAdapter(arrayAdapter);
 
+        degreeLevel = view.findViewById(R.id.degreeLevel);
+        arrayAdapter = new ArrayAdapter<>(view.getContext(),R.layout.list_item,degree);
+        degreeLevel.setAdapter(arrayAdapter);
+
+        batchNumber = view.findViewById(R.id.batchNumber);
+        arrayAdapter = new ArrayAdapter<>(view.getContext(),R.layout.list_item,batch);
+        batchNumber.setAdapter(arrayAdapter);
+
+        btnLogin = view.findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(view1 -> {
             loginFragment = new LoginFragment();
            replaceFragment(loginFragment);
+        });
+
+        universityId = view.findViewById(R.id.universityId);
+        firstName = view.findViewById(R.id.firstname);
+        lastName = view.findViewById(R.id.lastname);
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+        confirmPassword = view.findViewById(R.id.confirmPassword);
+        rdbLecturer = view.findViewById(R.id.rdbLecturer);
+        rdbStudent = view.findViewById(R.id.rdbStudent);
+
+        if(rdbStudent.isChecked())
+        {
+            userRole = "Student";
+        }
+        else if(rdbLecturer.isChecked())
+        {
+            userRole = "Lecturer";
+        }
+        courseBorder = view.findViewById(R.id.courseBorder);
+        degreeLevelBorder = view.findViewById(R.id.degreeLevelBorder);
+        batchNumberBorder = view.findViewById(R.id.batchNumberBorder);
+
+        rdbLecturer.setOnClickListener(view1 -> {
+            if(rdbLecturer.isChecked())
+            {
+                course.setVisibility(View.GONE);
+                courseBorder.setVisibility(View.GONE);
+
+                batchNumber.setVisibility(View.GONE);
+                batchNumberBorder.setVisibility(View.GONE);
+
+                degreeLevel.setVisibility(View.GONE);
+                degreeLevelBorder.setVisibility(View.GONE);
+
+                userRole = "Lecturer";
+            }
+            else if(rdbStudent.isChecked())
+            {
+                course.setVisibility(View.VISIBLE);
+                courseBorder.setVisibility(View.VISIBLE);
+
+                batchNumber.setVisibility(View.VISIBLE);
+                batchNumberBorder.setVisibility(View.VISIBLE);
+
+                degreeLevel.setVisibility(View.VISIBLE);
+                degreeLevelBorder.setVisibility(View.VISIBLE);
+
+                userRole = "Student";
+            }
+        });
+
+        rdbStudent.setOnClickListener(view1 -> {
+            if(rdbStudent.isChecked())
+            {
+                course.setVisibility(View.VISIBLE);
+                courseBorder.setVisibility(View.VISIBLE);
+
+                batchNumber.setVisibility(View.VISIBLE);
+                batchNumberBorder.setVisibility(View.VISIBLE);
+
+                degreeLevel.setVisibility(View.VISIBLE);
+                degreeLevelBorder.setVisibility(View.VISIBLE);
+
+                userRole = "Student";
+            }
+            else if(rdbLecturer.isChecked())
+            {
+                course.setVisibility(View.GONE);
+                courseBorder.setVisibility(View.GONE);
+
+                batchNumber.setVisibility(View.GONE);
+                batchNumberBorder.setVisibility(View.GONE);
+
+                degreeLevel.setVisibility(View.GONE);
+                degreeLevelBorder.setVisibility(View.GONE);
+
+                userRole = "Lecturer";
+            }
+        });
+
+        btnRegister = view.findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(view1 -> {
+            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(getActivity());
+            databaseAccess.open();
+            if(password.getEditText().getText().toString().equals(confirmPassword.getEditText().getText().toString()))
+            {
+                if(isValidUniveristyID(universityId) && checkFieldEmpty(firstName)&&checkFieldEmpty(lastName) && emailValidator(email) && checkDropDownEmpty(course)&&checkDropDownEmpty(degreeLevel)&&checkDropDownEmpty(batchNumber)&&isValidPassword(password))
+                {
+                    boolean result = false;
+
+                    try {
+                        result = databaseAccess.register(universityId.getEditText().getText().toString(),
+                        firstName.getEditText().getText().toString(),
+                        lastName.getEditText().getText().toString(),email.getEditText().getText().toString(),
+                        password.getEditText().getText().toString(),
+                        userRole,
+                        course.getText().toString(),degreeLevel.getText().toString(),batchNumber.getText().toString(),status);
+                    } catch (GeneralSecurityException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(result)
+                    {
+                        Toast.makeText(getActivity(),"Registration Success!",Toast.LENGTH_SHORT).show();
+                        loginFragment = new LoginFragment();
+                        replaceFragment(loginFragment);
+
+                    }
+                    else
+                    {
+                        Toast.makeText(getActivity(),"Registration Failed. You may already " +
+                                "registered!",Toast.LENGTH_SHORT).show();
+                    }
+
+                    databaseAccess.close();
+                }
+
+            }
+            else
+            {
+                confirmPassword.setError("Passwords do not match!");
+            }
+
+
         });
         return view;
     }
@@ -37,5 +198,95 @@ public class RegisterFragment extends Fragment {
         fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.homeFrameLayout,fragment);
         fragmentTransaction.commit();
+    }
+    public boolean isValidPassword(TextInputLayout textField) {
+        validPassword = false;
+        String password = textField.getEditText().getText().toString();
+        Pattern pattern;
+        Matcher matcher;
+        final String PasswordPattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+        pattern = Pattern.compile(PasswordPattern);
+        matcher = pattern.matcher(password);
+
+        if(matcher.matches()){
+            textField.setError(null);
+            validPassword = true;
+
+        }else{
+            textField.setError("Password should be more than 8 characters and contain Uppercase, Symbol and Number");
+            validPassword = false;
+
+        }
+        return validPassword;
+    }
+
+    public boolean isValidUniveristyID(TextInputLayout textField) {
+        validUniversityID = false;
+        String password = textField.getEditText().getText().toString();
+        Pattern pattern;
+        Matcher matcher;
+        final String PasswordPattern = "(^[a-zA-Z][0-9]{7}$)";
+        pattern = Pattern.compile(PasswordPattern);
+        matcher = pattern.matcher(password);
+
+        if(matcher.matches()){
+            textField.setError(null);
+            validUniversityID = true;
+
+        }else{
+            textField.setError("University ID must be in correct format");
+            validUniversityID = false;
+
+        }
+        return validUniversityID;
+    }
+    public boolean checkDropDownEmpty(AutoCompleteTextView autoCompleteTextView){
+        validDropdown = false;
+        if(autoCompleteTextView.getVisibility() == View.VISIBLE){
+            if(!autoCompleteTextView.getText().toString().isEmpty())
+            {
+                autoCompleteTextView.setError(null);
+                validDropdown = true;
+            }
+            else
+            {
+                autoCompleteTextView.setError("Please select the value");
+                validDropdown = false;
+            }
+        }
+        else
+        {
+            validDropdown = true;
+        }
+        return validDropdown;
+    }
+    public boolean emailValidator(TextInputLayout textField){
+        validEmail = false;
+        String email = textField.getEditText().getText().toString();
+        if(Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            textField.setError(null);
+            validEmail = true;
+        }
+        else
+        {
+            textField.setError("Please fill the correct value");
+            validEmail = false;
+        }
+        return validEmail;
+    }
+    public boolean checkFieldEmpty(TextInputLayout textField){
+        validField = false;
+        if(!textField.getEditText().getText().toString().isEmpty())
+        {
+            textField.setError(null);
+            validField = true;
+        }
+        else
+        {
+            textField.setError("Please fill the value");
+            validField = false;
+        }
+        return validField;
     }
 }
